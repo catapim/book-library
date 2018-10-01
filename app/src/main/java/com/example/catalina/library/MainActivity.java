8a73633cf82e28357e54af5dd3ce1b9b205f8af5
catalina <catalina@mego.games>
2018-10-01 20:02:55 -0300

    scroll fix

--- app/src/main/java/com/example/catalina/library/MainActivity.java
+++ app/src/main/java/com/example/catalina/library/MainActivity.java
@@ -1,37 +1,22 @@
 package com.example.catalina.library;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.librosBtn:
-                    setContentView(R.layout.activity_main);
-                    return true;
-                case R.id.salirBtn:
-                    finish();
-                    System.exit(0);
-                    return true;
-                case R.id.verButton:
-                    setContentView(R.layout.activity_main);
-            }
-            return false;
-        }
-
-
-    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
+
 
